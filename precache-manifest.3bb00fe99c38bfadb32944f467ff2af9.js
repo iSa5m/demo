97651942a57206d39d1066f041f5aaf4571bd99f
Soumyadip Pal <soumyadip.pal@infosys.com>
2020-02-03 09:45:51 +0530

    Updates

---
+++ precache-manifest.3bb00fe99c38bfadb32944f467ff2af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44aafd9d668b41007030a107484296c",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "b76911039860a142e6eb",
+    "url": "/demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9b3f6063591b507eed94",
+    "url": "/demo/static/js/2.dc845d7d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/demo/static/js/2.dc845d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76911039860a142e6eb",
+    "url": "/demo/static/js/main.712e9538.chunk.js"
+  },
+  {
+    "revision": "28b0b43f23e1107e2c5d",
+    "url": "/demo/static/js/runtime-main.b8243c2d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
